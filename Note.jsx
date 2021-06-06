5b0a57edef843d6b56c9bef648b88742dc10ec3a
S.Keshav Kumar <84336683+Keshav7476@users.noreply.github.com>
2021-06-06 18:57:09 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>Learn Artificial Intelligence and Full-Stack Web Development from people at Google, Goldman Sachs, American Express, Jio and Microsoft.</p>
+    </div>
+  );
+}
+
+export default Note;
